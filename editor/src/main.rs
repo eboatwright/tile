c21411e268a10955702aa34323884937880c15e3
eboatwright <eboatwright7236@gmail.com>
2022-02-12 13:42:00 -0600

    2/12/22 1:42 PM
     - added screen scaling!

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,16 +1,51 @@
 use tile::*;
 use macroquad::prelude::*;
 
-#[macroquad::main("Tile Editor - v0.0.0")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Tile Editor - v0.0.0".to_string(),
+        window_width: 960,
+        window_height: 600,
+        window_resizable: true,
+        ..Default::default()
+    }
+}
+
+#[macroquad::main(window_conf)]
 async fn main() {
+    let screen = render_target(960u32, 600u32);
+    screen.texture.set_filter(FilterMode::Nearest);
+    let camera = Camera2D {
+        zoom: vec2(1.0 / 960.0 * 2.0, 1.0 / 600.0 * 2.0),
+        render_target: Some(screen),
+        ..Default::default()
+    };
+
     let tilemap = Tilemap {
         texture: load_texture("tileset.png").await.unwrap(),
         tile_size: 16,
         tiles: load_tilemap("current.tilemap".to_string()).await,
     };
+
     loop {
+        set_camera(&camera);
+
         clear_background(BLACK);
         tilemap.render(TilemapRenderParams::default());
+
+        set_default_camera();
+
+        draw_texture_ex(
+            screen.texture,
+            -960.0,
+            -600.0,
+            WHITE,
+            DrawTextureParams {
+                dest_size: Some(vec2(960.0 * 2.0, 600.0 * 2.0)),
+                ..Default::default()
+            },
+        );
+
         next_frame().await
     }
 }
