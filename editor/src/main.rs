b06d68e074edb06694d6a1135b0e187b78b6e4ae
eboatwright <eboatwright7236@gmail.com>
2022-02-12 15:05:10 -0600

    2/12/22 3:05 PM
     - working on tile editor
        - added Master
        - updated screen scaling because i finally figured out how to use macroquad's camera system
        - seperated update and render into functions
        - added dragging with middle click

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,6 +1,12 @@
 use tile::*;
 use macroquad::prelude::*;
 
+struct Master {
+    tilemap: Tilemap,
+    camera_pos: Vec2,
+    mouse_down_pos: Vec2,
+}
+
 fn window_conf() -> Conf {
     Conf {
         window_title: "Tile Editor - v0.0.0".to_string(),
@@ -13,46 +19,53 @@ fn window_conf() -> Conf {
 
 #[macroquad::main(window_conf)]
 async fn main() {
-    let screen = render_target(960u32, 600u32);
-    screen.texture.set_filter(FilterMode::Nearest);
-    let camera = Camera2D {
-        zoom: vec2(1.0 / 960.0 * 2.0, 1.0 / 600.0 * 2.0),
-        render_target: Some(screen),
+    let mut camera = Camera2D {
+        zoom: vec2(1.0 / 480.0 * 2.0, -(1.0 / 300.0 * 2.0)),
         ..Default::default()
     };
 
-    let tilemap = load_tilemap("current.tilemap".to_string()).await;
+    let mut master = Master {
+        tilemap: load_tilemap("current.tilemap".to_string()).await,
+        camera_pos: Vec2::ZERO,
+        mouse_down_pos: Vec2::ZERO,
+    };
 
     loop {
+        update(&mut master);
+
+        camera.target = master.camera_pos.round();
         set_camera(&camera);
 
-        clear_background(BLACK);
-        for y in 0..tilemap.tiles[0].len() {
-            for x in 0..tilemap.tiles[0][0].len() {
-                draw_rectangle_lines(
-                    x as f32 * tilemap.tile_size as f32,
-                    y as f32 * tilemap.tile_size as f32,
-                    tilemap.tile_size as f32 + 1.0, tilemap.tile_size as f32 + 1.0,
-                    1.05,
-                    LIGHTGRAY,
-                );
-            }
-        }
-        tilemap.render(TilemapRenderParams::default());
+        render(&master);
 
-        set_default_camera();
-
-        draw_texture_ex(
-            screen.texture,
-            -960.0,
-            -600.0,
-            WHITE,
-            DrawTextureParams {
-                dest_size: Some(vec2(960.0 * 2.0, 600.0 * 2.0)),
-                ..Default::default()
-            },
-        );
-
-        next_frame().await
+        next_frame().await        
     }
+}
+
+fn update(master: &mut Master) {
+    if is_key_pressed(KeyCode::Space) {
+        master.camera_pos = Vec2::ZERO;
+    }
+    if is_mouse_button_pressed(MouseButton::Middle) {
+        master.mouse_down_pos = master.camera_pos + vec2(480.0, 300.0) + Vec2::from(mouse_position()) * 0.5;
+    }
+    if is_mouse_button_down(MouseButton::Middle) {
+        master.camera_pos = master.mouse_down_pos - vec2(480.0, 300.0) - Vec2::from(mouse_position()) * 0.5;
+    }
+}
+
+fn render(master: &Master) {
+    clear_background(BLACK);
+    for y in 0..master.tilemap.tiles[0].len() {
+        for x in 0..master.tilemap.tiles[0][0].len() {
+            draw_rectangle_lines(
+                x as f32 * master.tilemap.tile_size as f32,
+                y as f32 * master.tilemap.tile_size as f32,
+                master.tilemap.tile_size as f32 + 1.0, master.tilemap.tile_size as f32 + 1.0,
+                1.05,
+                LIGHTGRAY,
+            );
+        }
+    }
+    master.tilemap.render(TilemapRenderParams::default());
 }
